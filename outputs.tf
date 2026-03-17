93b809cc6b6108528a7d664cca2312d7fb62a274
smartao <smartao@gmail.com>
2026-03-17 16:25:01 -0300

    feat: Atualizado output para expor bloco cidr das subnets criadas

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,13 @@ output "nat_gateway_ids" {
   description = "Map of NAT Gateway IDs by Availability Zone"
   value       = { for az, nat in aws_nat_gateway.main : az => nat.id }
 }
+
+output "public_subnet_cidr_blocks" {
+  description = "List of public subnet CIDR blocks"
+  value       = [for az in var.azs : aws_subnet.public[az].cidr_block]
+}
+
+output "private_subnet_cidr_blocks" {
+  description = "List of private subnet CIDR blocks"
+  value       = [for az in var.azs : aws_subnet.private[az].cidr_block]
+}
